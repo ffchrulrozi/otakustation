b603b4bf87e4ff37aab01f1ef02f376b5c3449f1
fachrul rozi <ffchrulrozi@gmail.com>
2023-04-12 18:19:31 +0900

    first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,88 @@
+import { AppBar, Avatar, Badge, Box, Card, CardActionArea, CardContent, CardMedia, Checkbox, Divider, FormControl, FormControlLabel, FormGroup, FormLabel, IconButton, List, ListItem, ListItemAvatar, ListItemText, Radio, RadioGroup, TextField, Toolbar, Typography } from '@mui/material'
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { useState } from 'react'
 
 export default function Home() {
+  interface ITop {
+    title: string;
+    rating: number;
+    voterTotal: number;
+    img: string;
+  }
+
+  interface ILatest {
+    title: string;
+    img: string;
+    url: string;
+  }
+
+  interface INew {
+    title: string;
+    url: string;
+    img: string;
+    mindesc: string;
+  }
+
+  const [tops] = useState<ITop[]>([
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' },
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' },
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' },
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' },
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' },
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' },
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' },
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' },
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' },
+    { title: 'Naruto', rating: 8, voterTotal: 10, img: '' }
+  ]);
+
+  const [latests] = useState<ILatest[]>([
+    { title: 'Asd', img: '', url: '' },
+    { title: 'Asd', img: '', url: '' },
+    { title: 'Asd', img: '', url: '' },
+    { title: 'Asd', img: '', url: '' },
+    { title: 'Asd', img: '', url: '' },
+    { title: 'Asd', img: '', url: '' },
+    { title: 'Asd', img: '', url: '' },
+    { title: 'Asd', img: '', url: '' },
+    { title: 'Asd', img: '', url: '' },
+    { title: 'Asd', img: '', url: '' },
+  ])
+
+  const [infos] = useState<INew[]>([
+    {
+      title: 'Ad do eiusmod sunt aliqua laboris quis dolor aute reprehenderit aliquip duis dolor culpa.',
+      mindesc: 'Tempor voluptate voluptate cillum labore officia sunt cillum esse eu nisi.',
+      img: 'asd',
+      url: ''
+    },
+    {
+      title: 'Ad do eiusmod sunt aliqua laboris quis dolor aute reprehenderit aliquip duis dolor culpa.',
+      mindesc: 'Tempor voluptate voluptate cillum labore officia sunt cillum esse eu nisi.',
+      img: 'asd',
+      url: ''
+    },
+    {
+      title: 'Ad do eiusmod sunt aliqua laboris quis dolor aute reprehenderit aliquip duis dolor culpa.',
+      mindesc: 'Tempor voluptate voluptate cillum labore officia sunt cillum esse eu nisi.',
+      img: 'asd',
+      url: ''
+    },
+    {
+      title: 'Ad do eiusmod sunt aliqua laboris quis dolor aute reprehenderit aliquip duis dolor culpa.',
+      mindesc: 'Tempor voluptate voluptate cillum labore officia sunt cillum esse eu nisi.',
+      img: 'asd',
+      url: ''
+    },
+    {
+      title: 'Ad do eiusmod sunt aliqua laboris quis dolor aute reprehenderit aliquip duis dolor culpa.',
+      mindesc: 'Tempor voluptate voluptate cillum labore officia sunt cillum esse eu nisi.',
+      img: 'asd',
+      url: ''
+    }
+  ]);
+
   return (
     <>
       <Head>
@@ -14,110 +91,125 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className="main">
+        <section className="cover">
+          <div className='flex justify-between py-5 px-20 bg-blue-400'>
+            <div className='flex gap-10'>
+              <p>Anime</p>
+              <p>Manga</p>
+            </div>
+            <div>LOGIN</div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+        </section>
+        <section className="px-32 grid grid-cols-6">
+          <div className="col-span-1">
+            <Card className="p-5">
+              <FormGroup>
+                <FormLabel id="demo-radio-buttons-group-label">Genres</FormLabel>
+                <FormControlLabel control={<Checkbox />} label="Action" />
+                <FormControlLabel control={<Checkbox />} label="Adventure" />
+                <FormControlLabel control={<Checkbox />} label="Comedy" />
+                <FormControlLabel control={<Checkbox />} label="Drama" />
+                <FormControlLabel control={<Checkbox />} label="Fantasy" />
+                <FormControlLabel control={<Checkbox />} label="Horror" />
+                <FormControlLabel control={<Checkbox />} label="Isekai" />
+                <FormControlLabel control={<Checkbox />} label="Mystery" />
+                <FormControlLabel control={<Checkbox />} label="School" />
+                <FormControlLabel control={<Checkbox />} label="Sci-Fi" />
+                <FormControlLabel control={<Checkbox />} label="Supernatural" />
+              </FormGroup>
+            </Card>
           </div>
-        </div>
+          <div className="col-span-5 px-5">
+            <div className='flex justify-center'>
+              <Box
+                sx={{
+                  width: 500,
+                  maxWidth: '100%',
+                  background: 'white'
+                }}
+              >
+                <TextField fullWidth placeholder="Search..." />
+              </Box>
+            </div>
+            <div className="grid grid-cols-5 gap-3">
+              {latests.map((latest, latestKey) => (
+                <Card sx={{ maxWidth: 345 }} key={latestKey}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image="/assets/img/box.png"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <p>Black Clover</p>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </section>
+        <section className='px-32'>
+          <div className="grid grid-cols-3">
+            <div className="col-span-2">
+              {infos.map((info, infoKey) => infoKey == 0 ? (
+                <Card className='grid grid-cols-5 mb-5' key={infoKey}>
+                  <div className='col-span-2'>
+                    <div style={{ background: 'red', width: '100%', height: 300 }}></div>
+                  </div>
+                  <div className="col-span-3">
+                    In eiusmod incididunt aliqua fugiat eu enim aute.
+                  </div>
+                </Card>
+              ) : (
+                <Card className='grid grid-cols-5 mb-5' key={infoKey}>
+                  <div className='col-span-1'>
+                    <div style={{ background: 'red', width: '100%', height: 150 }}></div>
+                  </div>
+                  <div className="col-span-4">
+                    In eiusmod incididunt aliqua fugiat eu enim aute.
+                  </div>
+                </Card>
+              ))}
+            </div>
+            <div className="col-span-1">
+              Viral
+              <Card className='top-anime'>
+                <h2>10 Top Anime</h2>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                  {tops.map((top, topKey) => (
+                    <ListItem alignItems="flex-start" key={topKey}>
+                      <ListItemAvatar>
+                        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary="Brunch this weekend?"
+                        secondary={
+                          <>
+                            <Typography
+                              sx={{ display: 'inline' }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              Ali Connors
+                            </Typography>
+                            {" — I'll be in your neighborhood doing errands this…"}
+                          </>
+                        }
+                      />
+                    </ListItem>
+                  ))}
+                </List>
+              </Card>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            </div>
+          </div>
+        </section >
+        <footer></footer>
+      </main >
     </>
   )
 }
