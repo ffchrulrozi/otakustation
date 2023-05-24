26bf5e83e0788d09220c3c7d2a5bb5278a9393f8
fachrul rozi <ffchrulrozi@gmail.com>
2023-05-24 18:03:01 +0900

    minor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
-import {useRouter} from "next/router";
-import { useEffect } from "react";
 import LeftBar from "./_/LeftBar";
 
 export default function Anime() {
-    const router = useRouter();
-    useEffect(()=>{
-        console.log(router.route)
-    },[])
+    
     return (
         <>
             <section className="grid grid-cols-6">
